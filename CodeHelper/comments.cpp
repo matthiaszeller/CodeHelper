88840eefcd84c1cf80ab4ee0e985eafc87662528
Matthias <m.zeller1901@gmail.com>
2019-12-25 00:04:41 +0100

    comment style... to complete

--- CodeHelper/comments.cpp
+++ CodeHelper/comments.cpp
@@ -25,9 +25,17 @@ QString Comments::get_block_comment(QString text, int length) const {
 
 	QString l1 = c + side + m_FillingChar.repeated(length -
 					2*side.size() - 2*c.size()) + side + c2;
-	t = l1 + "\n";
-    t += getMiddleLine(side, text, c, c2, length);
-	t += "\n" + l1;
+    if(m_Style == Default) {
+        t = l1 + "\n";
+        t += getMiddleLine(side, text, c, c2, length);
+        t += "\n" + l1;
+    } else if(m_Style == SingleLine) {
+        t += getMiddleLine(side, text, c, c2, length);
+    } else {
+        t = c + "\n" + l1 + "\n";
+        t += getMiddleLine(side, text, c, c2, length);
+        t += "\n" + l1 + "\n" + c;
+    }
 
 	return t;
 }
@@ -42,6 +50,8 @@ QString Comments::getMiddleLine(const QString &side, const QString &text,
     // Compute the remaining char number to print
     int rem(side.size() + c2.size() + N_side);
     int residual(length - t.size() - rem);
+    // Must adapt the number of filling charaters if there is an excess/lack of chars
+    // Why residuals are > 1 and not > 0 is wrong not adapted ? Too lazy to check, but it works
     if(residual > 1) N_side += 1;
     t += m_FillingChar2.repeated(N_side) + side + c2;
 
