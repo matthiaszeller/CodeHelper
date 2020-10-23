44184576cd8ad6219f6756f8d71fc7e1b2e694ee
Matt <m.zeller1901@gmail.com>
2020-10-23 02:55:37 +0200

    reformat code and support Markdown files

--- gen_contents.py
+++ gen_contents.py
@@ -1,4 +1,4 @@
-"""Generate a contents table of a jupyter notebook.
+"""Generate a contents table of a jupyter notebook or a Markdown file.
 This scripts outputs to standard output, can simply copy and paste it in a markdown cell.
 
 Example of usage (get a numbered list):
@@ -24,77 +24,123 @@ def main():
 
     args = parser.parse_args()
 
-    reader = NotebookReader(args.file)
+    # Sanity check
+    if args.file.endswith('.ipynb'):
+        reader = NotebookReader(args.file)
+    elif args.file.endswith('.md'):
+        reader = MarkdownReader(args.file)
+    else:
+        raise ValueError('Unrecognized file')
+
     titles = reader.get_titles()
 
     contents = reader.generate_contents_table(titles,
                                               numbered=not args.bullet,
-                                              max_level=args.level)
+                                              max_levels=args.level)
     print(contents)
 
 
-class NotebookReader:
+class Reader:
+
+    def __init__(self, fp):
+        with open(fp, 'r') as f:
+            self.data = self._load_data(f)
+
+    @staticmethod
+    def _load_data(file):
+        pass
+
+    def get_titles(self):
+        pass
+
+    @classmethod
+    def generate_contents_table(cls, titles, numbered=True, max_levels=3):
+        """
+        :param titles: list of tuples (str title, int level)
+        :param bool numbered: whether to number items or use bullet points
+        :param int max_levels: maximum title level to include in contents table (i.e. number of `#`)
+        :return: contents table (string) that can be copied in a markdown cell
+        """
+        pass
+
+
+class MarkdownReader(Reader):
 
     HTML_LINK_REPLACE_MAPPING = str.maketrans({
         '`': '', ' ': '-'
     })
 
-    def __init__(self, fp):
-        with open(fp, 'r') as f:
-            self.data = json.load(f)
+    def get_titles(self):
+        """
+        :return: list of (str title, int level) in the order found in file
+        """
+        source = self.data.split('\n')
+        return self._detect_headers(source)
 
     @classmethod
-    def generate_contents_table(cls, titles, numbered=True, max_level=3):
+    def generate_contents_table(cls, titles, numbered=True, max_levels=3):
         """
         :param titles: list of tuples (str title, int level)
         :param bool numbered: whether to number items or use bullet points
-        :param int max_level: maximum title level to include in contents table (i.e. number of `#`)
+        :param int max_levels: maximum title level to include in contents table (i.e. number of `#`)
         :return: contents table (string) that can be copied in a markdown cell
         """
         bullet = '1.' if numbered else '*'
 
-        def format_title_name(name):
-            # Sanitize characters
-            name = name.strip()
-            name = name.translate(cls.HTML_LINK_REPLACE_MAPPING)
-            return name
-
-        def create_link_entry(title):
-            link = format_title_name(title)
-            return f'[{title}](#{link})'
-
-        def gen_contents_item(title, level):
-            indent = '    ' * (level - 1)
-            return indent + f'{bullet} ' + create_link_entry(title)
-
         entries = [
-            gen_contents_item(*e) for e in titles
-            if e[1] <= max_level
+            cls._gen_contents_item(*e, bullet) for e in titles
+            if e[1] <= max_levels
         ]
 
         return '\n'.join(entries)
 
+    @classmethod
+    def _gen_contents_item(cls, title, level, bullet):
+        def format_title_name(name):
+            """Sanitize characters"""
+            return name.translate(cls.HTML_LINK_REPLACE_MAPPING)
+
+        def create_link_entry(title):
+            title = title.strip()
+            link = format_title_name(title)
+            return f'[{title}](#{link})'
+
+        indent = '    ' * (level - 1)
+        return f'{indent} {bullet} {create_link_entry(title)}'
+
+    @staticmethod
+    def _detect_headers(markdown_source):
+        match_pattern = map(
+            lambda src: re.search(r'^\#+ (.+)\n?', src),
+            markdown_source
+        )
+        # Discard None's
+        match_pattern = filter(lambda e: e is not None, match_pattern)
+        # Extract title and title level
+        return [
+            (pat.group(1), pat.group(0).split(' ')[0].count('#'))
+            for pat in match_pattern
+        ]
+
+    @staticmethod
+    def _load_data(file):
+        return file.read()
+
+
+class NotebookReader(MarkdownReader):
+
+    @staticmethod
+    def _load_data(file):
+        return json.load(file)
+
     def get_titles(self):
         """
-        :return: list of (str title, int level)
+        :return: list of (str title, int level) in the order found in file
         """
-        def detect_headers(markdown_source):
-            match_pattern = map(
-                lambda src: re.search(r'^\#+ (.+)\n?', src),
-                markdown_source
-            )
-            # Discard None's
-            match_pattern = filter(lambda e: e is not None, match_pattern)
-            # Extract title and title level
-            return [
-                (pat.group(1), pat.group(0).split(' ')[0].count('#'))
-                for pat in match_pattern
-            ]
-
         markdown = self.get_markdown_cells()
         titles = []
         for cell in markdown:
-            headers = detect_headers(cell['source'])
+            headers = self._detect_headers(cell['source'])
             titles.extend(headers)
 
         return titles
